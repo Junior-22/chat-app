4ae05966e9a4d58a2985d543c351f8cd40f37425
Junior-22 <juniorfrancis2608@gmail.com>
2022-07-11 21:24:30 +0200

    Create app.js

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import "react-native-gesture-handler"
+// import react navigation
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+// import navigation screens
+import Home from "./components/Home";
+import Chat from "./components/Chat";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// create the navigator
+const Stack = createStackNavigator();
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: "" };
+  }
+
+  render() {
+
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+
+          <Stack.Screen
+            name="Home"
+            component={Home}
+          />
+
+          <Stack.Screen
+            name="Chat"
+            component={Chat}
+          />
+
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
